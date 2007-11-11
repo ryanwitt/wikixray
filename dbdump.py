2550fdcc78aa93dbc5158a7e1aac67a1939835dc
gfenix <gfenix@fa2d0aff-2b27-0410-99f4-e39b7df3edc3>
2007-11-11 16:24:01 +0000

    new dbdump object oriented

--- dbdump.py
+++ dbdump.py
@@ -12,10 +12,9 @@
 # Author: Jose Felipe Ortega Soto                                                             
 
 """
-Module for downloading database dump for each langauge version we want to 
+Module for downloading database dump of any of the langauge editions we want to 
 analyze. Once downloaded in 7zip format (storage optimization), we decompress the
 dump to a local MySQL database.
-
 This way, we have prepared the database for the next steps in the quantitative
 analysis process.
 
@@ -30,96 +29,126 @@ analysis process.
 
 import os, datetime, dbaccess
 
-def download_bd (conf, language="furwiki"):
-	# Recibe como parámetro el idioma que queremos descargar
-	# La funcion usará w-get para descubrir la última versión correcta del volcado de ese idioma
-	
-	str_types={"dump_pages":"pages-meta-history.xml.7z", "dump_stub":"stub-meta-history.xml.gz"}
-	
-	urld="http://download.wikimedia.org/"+language+"/latest/"+language+"-latest-"+str_types.get(conf.dumptype)
-	urlp="http://download.wikimedia.org/"+language+"/latest/"+language+"-latest-page.sql.gz"
-	#pattern="http://download.wikimedia.org/furwiki/20060921/furwiki-20060921-pages-meta-history.xml.7z"
-	#http://download.wikimedia.org/amwiki/20061014/amwiki-20061014-stub-meta-history.xml.gz
-	
-	print "Tratando de recuperar el archivo... "+urld+"\n"
-	success=os.system("wget -P dumps -o log_"+language+" --ignore-length "+urld)
-	if success== 0:
-		print "Tratando de recuperar el archivo... "+urlp+"\n"
-		ok=os.system("wget -P dumps -o log_"+language+" --ignore-length "+urlp)
-		if ok==0:
-			return (language+"-latest-"+str_types.get(conf.dumptype))
-		else:
-			print "Fallo en la recuperacion del archivo de datos de paginas... "+language
-	else:
-		print "FALLO EN recuperacion del archivo latest para el lenguage --- "+language	
-	print "status= %i" % success	
-
-def unzip_bd (conf, filename, language="furwiki"):
-    # Recibe como parámetro el idioma que queremos descomprimir
-    ok=__init_bd(conf, language)
-    ##    ok=0
-    #print "traza ok = %i \n" % ok	
-    if ok == 0:
-        print("BD inicializada, descomprimiendo archivos...")
-        if conf.dumptype=="dump_pages":
-            # La funcion llama al script de descompresión y pasa a traves de un pipe la info a mwdumper y de ahí a MySQL
-            command="7za e -so dumps/"+filename+ " | "+conf.jpath+" -server -jar "+conf.mwpath+" --format=sql:1.5 | mysql -u "+conf.msqlu+" -p"+conf.msqlp+" "+language+conf.dumptype.lstrip("dump")
-            success=os.system(command)
-            if success == 0:
-                print "BD descomprimida para el idioma ... "+language+conf.dumptype.lstrip("dump")+"\n\n"
-            else:
-                print "Ocurrio algun problema al descomprimir BD del idioma ... "+language+conf.dumptype.lstrip("dump")+"\n\n"
-        elif conf.dumptype=="dump_stub":
-            print("Decompressing gzip file...\n\n")
-            command_unzip="gzip -d dumps/"+filename
-            success=os.system(command_unzip)
-##            success=0
-            if success == 0:
-                print("Initiating mwdumper decompression...")
-                command_decompress=conf.jpath+" -server -jar "+conf.mwpath+" --format=sql:1.5 dumps/"+filename.replace(".gz", "")+" | mysql -u "+conf.msqlu+" -p"+conf.msqlp+" "+language+conf.dumptype.lstrip("dump")
-                succ=os.system(command_decompress)
-                if succ == 0:
-                    print "BD descomprimida para el idioma ... "+language+conf.dumptype.lstrip("dump")+"\n\n"
-                    command_zip="gzip dumps/"+filename.replace(".gz","")
-                    os.system(command_zip)
-                else:
-                    print "Ocurrio algun problema al descomprimir BD del idioma ... "+language+conf.dumptype.lstrip("dump")+"\n\n"
-            else:
-                print "Problema de descompresion del archivo .zip\n"
-                
+class dump(object):
+    """
+    A class modelling dumps object for every Wikipedia's database dump we process
+    """
+    def __init__(self, language="furwiki", dumptype="research", msqlu="", msqlp=""):
+        """
+        It receives the language and dumptype to download
+        It returns an int =0 if the DB was successfully set up, =-1 if there was an error
+        """
+        self.language=language       #language to download
+        self.dumptype=dumptype      #type of dump      
+        self.filename=""        #dump's filename in Wikimedia's server
+        if (msqlu=="" or msqlp==""):
+            print "Error initializing DB dump object. You must provide a valid MySQL username and password"
+            return -1
         else:
-            print "Error en la seleccion del tipo de archivo de descompresion"
-    else:
-        print "Error de inicialización de la BD\n"
-
-def __init_bd(conf, language="furwiki", dump_type="dump_stub"):
-    # Construye la BD en MySQL para poder importar el dump del idioma indicado como argumento
+            self.msqlu=msqlu   #MySQL username for accessing and editing the DB
+            self.msqlp=msqlp   #MySQL password
+        #We can manage two different types of dumps, stubs (without the text of every revision) and pages
+        #(containing the text of every revision)
+        self.urld="http://download.wikimedia.org/"+self.language+"/latest/"+\
+        self.language+"-latest-pages-meta-history.xml.7z"  #File to download
+        #patterns for files
+        #http://download.wikimedia.org/furwiki/20060921/furwiki-20060921-pages-meta-history.xml.7z
+        #http://download.wikimedia.org/amwiki/20061014/amwiki-20061014-stub-meta-history.xml.gz
+        
+        ## Initialize DB in MySQL: create DB and tables definitions
+        print "Initializing DB for --> "+ self.language +"\n"
+        #Retrieving connection and cursor to access the DB
+        acceso = dbaccess.get_Connection("localhost", 3306, self.msqlu, self.msqlp,"mysql")
+        dbaccess.createDB_SQL(acceso[1],"wx_"+self.language+"_"+self.dumptype)
+        if self.dumptype=="research":
+            command="mysql -u "+self.msqlu+" -p"+self.msqlp+" " +\
+            "wx_"+self.language+"_"+self.dumptype+" < tables_research.sql > debug_mysql.log"
+        elif self.dumptype=="standard":
+            command="mysql -u "+self.msqlu+" -p"+self.msqlp+" " +\
+            "wx_"+self.language+"_"+self.dumptype+" < tables_standard.sql > debug_mysql.log"
+        ok=os.system(command)
+        if ok == 0:
+            acceso = dbaccess.get_Connection("localhost", 3306, self.msqlu, self.msqlp,\
+            "wx_"+self.language+"_"+self.dumptype)
+            dbaccess.raw_query_SQL(acceso[1], "alter table page max_rows = 200000000000 avg_row_length = 50")
+            dbaccess.raw_query_SQL(acceso[1], "alter table revision max_rows = 200000000000 avg_row_length = 50")
+            if self.dumptype=="standard":
+                dbaccess.raw_query_SQL(acceso[1], "alter table text max_rows = 200000000000 avg_row_length = 50")
+            dbaccess.close_Connection(acceso[0])
+        else:
+            print "Error! There was a problem initializing definitions for DB tables"
+            dbaccess.close_Connection(acceso[0])
+            
+    def download_bd (self):
+        """
+        Downloads the necessary dump files from http://download.wikimedia.org
+        """
+        # Arguments are the self.language and dump type to download
+        # This method uses wget to discover the most up to date version for that type of dump in that language edition
+        print "Trying to recover dump file... "+self.urld+"\n"
+        success=os.system("wget -P dumps -o log_"+self.language+" --ignore-length "+self.urld)
+        if success== 0:
+            print "File successfully downloaded..."
+            self.filename=self.language+"-latest-pages-meta-history.xml.7z"
+            return success
+        else:
+            # Error retrieving main dump
+            print "There was an error recovering the main dump for the "+self.language+" language edition"
+            print "Most probably, the dump does not exist. Check on http://download.wikimedia.org."
+            return -1
     
-    print "Inicializando BD para "+ language +"\n"
-    acceso = dbaccess.get_Connection("localhost", 3306, conf.msqlu, conf.msqlp)
-    dbaccess.createDB_SQL(acceso[1],language+conf.dumptype.lstrip("dump"))
-    dbaccess.close_Connection(acceso[0])
-    print("Unzipping pages data...\n")
-    command_unzip="gzip -d dumps/"+language+"-latest-page.sql.gz"
-    os.system(command_unzip)
-    command1="mysql -u "+conf.msqlu+" -p"+conf.msqlp+" " + language+conf.dumptype.lstrip("dump") + " < dumps/"+language+"-latest-page.sql"+" > output.txt"
-    os.system(command1)
-    acceso = dbaccess.get_Connection("localhost", 3306, conf.msqlu, conf.msqlp, language+conf.dumptype.lstrip("dump"))
-    dbaccess.raw_query_SQL(acceso[1], "rename table page to aux")
-    dbaccess.close_Connection(acceso[0])
-    command_zip="gzip dumps/"+language+"-latest-page.sql"
-    print("Zipping pages data....\n")
-    os.system(command_zip)
-    command2="mysql -u "+conf.msqlu+" -p"+conf.msqlp+" " + language+conf.dumptype.lstrip("dump") + " < tables_15_09_2006.sql > output.txt"
-    ok=os.system(command2)
-    if ok == 0:
-        acceso = dbaccess.get_Connection("localhost", 3306, conf.msqlu, conf.msqlp, language+conf.dumptype.lstrip("dump"))
-        dbaccess.raw_query_SQL(acceso[1], "alter table page max_rows = 200000000000 avg_row_length = 50")
-        dbaccess.raw_query_SQL(acceso[1], "alter table revision max_rows = 200000000000 avg_row_length = 50")
-        dbaccess.raw_query_SQL(acceso[1], "alter table text max_rows = 200000000000 avg_row_length = 50")
+    def unzip_bd (self):
+        """
+        Decompress the DB dumps into MySQL
+        """	
+        if self.dumptype=="research":
+            #Then we call our parser "dump_sax_research.py"to load data into MySQL
+            command_7z="7za e -so dumps/"+self.filename+ " | "+"python dump_sax_research.py "+\
+            "-u "+self.msqlu+" -p "+self.msqlp+" -d "+"wx_"+self.language+"_"+self.dumptype
+            success=os.system(command_7z)
+            if success == 0:
+                print "DB "+"wx_"+self.language+\
+                self.dumptype+" successfully decompressed...\n\n"
+            else:
+                print "Error! There was an error trying to decompress database --> "+\
+                "wx_"+self.language+self.dumptype
+                
+        elif self.dumptype=="standard":
+            print("Initiating decompression and parsing...")
+            command_7z="7za e -so dumps/"+self.filename+ " | "+"python dump_sax.py "+\
+            "-u "+self.msqlu+" -p "+self.msqlp+" -d "+"wx_"+self.language+"_"+self.dumptype
+            succ=os.system(command_7z)
+            if succ == 0:
+                print "DB --> "+"wx_"+self.language+self.dumptype+\
+                " successfully decompressed\n\n"
+            else:
+                print "Error! There was an error decompressing DB ... "+\
+                "wx_"+self.language+self.dumptype
+        else:
+            print "Error! Unexpected type of dump received"
+            
+        print "Generating indexes for tables page and revision...\n"
+        print "Depending on the dump size this may take a while...\n"
+        acceso = dbaccess.get_Connection("localhost", 3306, self.msqlu,\
+        self.msqlp, "wx_"+self.language+"_"+self.dumptype)
+        #Generate adequate indexes and keys in tables page and revision
+        print "Generating index for page_len...\n"
+        dbaccess.raw_query_SQL(acceso[1],"ALTER TABLE page ADD INDEX (page_len)")
+        print "Modifying rev_timestamp to support DATETIME and creating index...\n"
+        dbaccess.raw_query_SQL(acceso[1],"ALTER TABLE revision MODIFY rev_timestamp DATETIME")
+        dbaccess.raw_query_SQL(acceso[1],"ALTER TABLE revision ADD INDEX timestamp (rev_timestamp)")
+        print "Generating index for rev_page and rev_timestamp...\n"
+        dbaccess.raw_query_SQL(acceso[1],"ALTER TABLE revision ADD INDEX page_timestamp(rev_page, rev_timestamp)")
+        print "Generating index for rev_user and rev_timestamp...\n"
+        dbaccess.raw_query_SQL(acceso[1],"ALTER TABLE revision ADD INDEX user_timestamp(rev_user, rev_timestamp)")
+        print "Generating index for rev_user_text and timestamp...\n"
+        dbaccess.raw_query_SQL(acceso[1],"ALTER TABLE revision ADD INDEX usertext_timestamp(rev_user_text(15), rev_timestamp)")
         dbaccess.close_Connection(acceso[0])
-        return ok
-    else:
-        print "Problema al crear la BD"
-        return -1
+        print "Database ready for quantitative analysis...\n"
+        print "Let's go on... Cross your fingers... ;-) \n\n\n"
+
+if __name__ == '__main__':
+    foobar=dump(dumptype="standard",msqlu="root", msqlp="phoenix")
+    foobar.download_bd()
+    foobar.unzip_bd()
 
